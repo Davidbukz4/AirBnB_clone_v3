79a8cabed5b77cba4755efd62e0a86f965ca910f
David Egwuatu <davidegwuatu2@gmail.com>
2023-04-30 00:24:37 +0000

    resolved task 4 error(i)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,24 @@ Status module
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status')
 def status_ok():
     ''' Returns status '''
     obj = {"status": "OK"}
     return jsonify(obj)
+
+
+@app_views.route('/stats')
+def status_all():
+    ''' Returns the number of each object by type '''
+    stat = {}
+    stat['amenities'] = storage.count('Amenity')
+    stat['cities'] = storage.count('City')
+    stat['places'] = storage.count('Place')
+    stat['reviews'] = storage.count('Review')
+    stat['states'] = storage.count('State')
+    stat['users'] = storage.count('User')
+    return jsonify(stat)
